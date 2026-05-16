fa06646372a28a681de721e2474fa1a8a3afe40f
Mrj4ck2005 <jackanderson.rg@gmail.com>
2026-05-16 16:34:53 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ValidaPE/', // Ajusta esto al nombre exacto de tu repositorio en GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
